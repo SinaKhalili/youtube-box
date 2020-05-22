f7f372e6f62916c602a630e7439111d406a3895c
sinakhalili <khalili@sfu.ca>
2020-05-21 21:34:19 -0700

    Fix os.environ call

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 hub_text = requests.get("https://api.github.com/zen").text
 
 print("hello")
-gh_code = os.environ("GH_TOKEN")
-yt_code = os.environ("YT_API_KEY")
+gh_code = os.environ["GH_TOKEN"]
+yt_code = os.environ["YT_API_KEY"]
 
 print(f"{gh_code} and {yt_code}")
