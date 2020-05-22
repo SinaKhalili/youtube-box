c8848a64b2e23a88a0701853a452a22a83995767
sinakhalili <khalili@sfu.ca>
2020-05-21 21:24:00 -0700

    Add workflow action and main.py
    The main.py is just there for testing right now

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import os
+
+hub_text = requests.get("https://api.github.com/zen").text
+
+print("hello")
+gh_code = os.environ("GH_TOKEN")
+yt_code = os.environ("YT_API_KEY")
+
+print(f"{gh_code} and {yt_code}")
